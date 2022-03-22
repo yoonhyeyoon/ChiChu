6c943999250a030883395014945018fd95e135b6
LWY <amethyst270@naver.com>
2022-03-22 17:39:32 +0900

    1차 검색 api filter

--- back/chichu/search/serializers.py
+++ back/chichu/search/serializers.py
@@ -1,5 +1,20 @@
+from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
+from django.views.generic.base import RedirectView
 from .models import DBOption, Company, ProductSubtype, Product, ProductOption, ProductRate, Contract, User, UserContract, ProductContract
 
-        
+
+'''
+1) 보험상품코드
+2) 담보코드
+3) 회사로고
+4) 보험상품명
+5) 설계유형
+6) 납입보험료 
+'''
+
+class Default(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('')
--- back/chichu/search/views.py
+++ back/chichu/search/views.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -5,26 +6,58 @@ from django.shortcuts import get_object_or_404
 from .models import DBOption, Company, ProductSubtype, Product, ProductOption, ProductRate, Contract, User, UserContract, ProductContract
 
 
-'''
-1) 보험상품코드
-2) 담보코드
-3) 회사 로고 
-4) 보험상품명
-5) 설계유형
-6) 납입보험료
-7) total_index 치츄
-8) product_rate > rate
-9) coverage sum 
-보험료순
-보장금액
-보장금액
-3가지 기준별 높은 순
-'''
-
 # 1차 검색
 @api_view(['GET'])
-def default(request):
-    pass 
+def default(request, age, gender):
+    # 1. 보험 나이로 바꾸기
+    today = datetime.today().strftime("%Y%m%d")
+    today_month = int(today[4:])
+    user_birthday = age[4:]
+
+    user_age = int(today) - int(age)
+
+    if today_month > user_birthday:
+        user_age + 1
+
+
+    # 2. SQL 명령어로 성별, 연령에 맞춰 filter 1한 값 sql  27살, 여성이다.
+    # PRODUCT_RATE.AGE == 'age' and PRODUCT_RATE.GENDER == 'gender' 인 product
+    
+    asap = []
+    popular = []
+    reasonable = []
+    
+    ''' SQL로 접근 
+    1) 지금 당장 보장
+    2) 해당 성별/연령에서 가장 인기 많은 순서
+    3) 가성비가 좋은 (PRODUCT_OPTION > PRODUCT_CODE > SUM COVERAGE 크면서, 납입보험료 가장 낮은 순서로 최대 몇개?
+    질문 1. 최대 몇개?
+    질문 2. 가성비가 좋은 = 보장금액 COVERAGE SUM  250만원 / 월 보험료 5만원 * 가입기간 10년    ㅇㅇ
+    질문 3. 보장금액 높은 순이 sum coverage일까요? 
+    
+    CSV 파일 넣어서 API 만들어보기 
+    '''
+    
+
+    # 3)  ORDERBY 보험 상품 LIST 만들기  
+
+    # 4) 성별, 연령, 기간 10년, 일반형 PRODUCT_RATE(PY == 10, PRODUCT(SUBTYPE_CODE == 1))
+    chichu = []
+    cheap = []
+    coverage_up = []
+    '''
+    치츄지수 높은 순 
+    보험료 낮은 순 
+    보장금액 높은 순
+    '''
+    # 5)  ORDERBY 보험 상품 LIST 만들기
+
+    # 6) 6가지 조건에 맞춰서 각 보험 상품에 대한 세부 정보들은 serializer에서 표시
+    # movies = Movie.objects.filter(title__icontains=word)
+    # serializer = MovieListSerializer(movies, many=True)
+    # return Response(serializer.data)
+
+
 
 # 2차 검색
 @api_view(['GET'])
