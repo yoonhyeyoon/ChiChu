f45f5ba30558d30cec0a3ff3c842ac6bad3690d4
LWY <amethyst270@naver.com>
2022-03-24 14:55:49 +0900

    2차 검색 api

--- back/chichu/search/serializers.py
+++ back/chichu/search/serializers.py
@@ -5,36 +5,31 @@ from django.views.generic.base import RedirectView
 from .models import DbOption, Company, ProductSubtype, Product, ProductOption, ProductRate, Contract, User, SearchCompany
 
 
-# search를 사용하는 페이지 :
+# search를 사용하는 페이지 
+# 1) 보험상품코드 (PRODUCT_CODE) 
+# 2) 담보코드 (OPTION_CODE) PRODUCT TABLE
+# 3) 회사로고 (COMPANY_LOGO) COMPANY TABLE
+# 4) 보험상품명 (PRODUCT_NAME) PRODUCT TABLE
+# 5) 설계유형 (SUBTYPE_CODE) PRODUCT TABLE 
+# 6) 납입보험료 (RATE) PRODUCT_RATE TABLE
+
+
 # 1. 검색결과
+class DefaultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductRate
+        fields = ('__all__')
+
+
 # 2. 상품비교
-# 3. 검색결과 상세페이지
+class CompareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductRate
+        fields = '__all__'
 
 
-# 1. 검색결과
-# 1-1. 보험상품코드 
-# 2) 담보코드
-# 3) 회사로고
-# 4) 보험상품명
-# 5) 설계유형
-# 6) 납입보험료 
-
-
-class Comment(object):
-    def __init__(self, email, content, created=None):
-        self.email = email
-        self.content = content
-        self.created = created or datetime.datetime.now()
-
-comment = Comment(email='leila@example.com', content='foo bar')
-
-
-class Default(serializers.ModelSerializer):
-
-    class Meta1:
-        model = Company
-        fields = ('')
-    
-    class Meta2:
-        model = Company
-        fields = ('')
+# 3. 상품 상세 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductRate, Contract
+        fields = '__all__'
--- back/chichu/search/views.py
+++ back/chichu/search/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from email.mime import application
+from h11 import PRODUCT_ID
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,46 +8,67 @@ from rest_framework import serializers
 from django.shortcuts import get_object_or_404
 import pymysql
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chichu.settings")
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chichu.settings")
 
-import django
-django.setup()
-
+# import django
+# django.setup()
 
 from .models import DbOption, Company, ProductSubtype, Product, ProductOption, ProductRate, Contract, User
+from .serializers import (
+    DefaultSerializer, ProductSerializer
+    )
 
-
-# 1차 검색
-@api_view(['GET'])
-def default(request):
-    #필요한 기본 DB 정보
-    host = "chichu" #접속할 db의 host명
-    user = "chichu" #접속할 db의 user명
-    pw = "ssafy" #접속할 db의 password
-    db = "chichu" #접속할 db의 table명 (실제 데이터가 추출되는 table)
-
-    #DB에 접속
-    conn = pymysql.connect( host= host, user = user, password = pw, db = db)
-    # Connection 으로부터 Cursor 생성 
-    curs = conn.cusor()
-
-
-    # 1. 사용자 나이를 보험 나이로 바꾸기
-    age = 19960225
-    user_gender = '여'
+# 1. 사용자 나이를 보험 나이로 바꾸기
+def change_age(age):
     today = datetime.today().strftime("%Y%m%d")
+    user_age = int(today) - int(age)
+
+    # 생일 지났는지 안지났는지 확인
     today_month = int(today[4:])
     user_birthday = age[4:]
 
-    user_age = int(today) - int(age)
     if today_month > user_birthday:
         user_age + 1
     
-    if user_age % 10 >=5:
-        user_age = (user_age // 10) * 10 + 5
-    else:
-        user_age = (user_age // 10) * 10
+    # 5단위로 나이 맞추기 
+    if user_age <= 20:
+        return 20
+    elif 20 < user_age <= 25:
+        return 25
+    elif 25 < user_age <= 30:
+        return 30
+    elif 30 < user_age <= 35:
+        return 35   
+    elif 35 < user_age <= 40:
+        return 40
+    elif 40 < user_age <= 45:
+        return 45
+    elif 45 < user_age <= 50:
+        return 50            
+    elif 50 < user_age <= 55:
+        return 55
+    elif 55 < user_age <= 60:
+        return 60
+    elif 60 < user_age <= 65:
+        return 65               
+    elif 65 < user_age <= 70:
+        return 70
+    elif 70 < user_age <= 75:
+        return 75
+    elif 75 < user_age <= 80:
+        return 80   
+
+# 1 - 1차 검색
+@api_view(['GET'])
+def default(request, age, gender):
+
+    # 예시
+    age = 19960225
+    gender = '여'
+    
+    # 1. 나이 바꾸기
+    age = change_age(age)
 
     # 2. 리스트에 담기
     # 2-1. (선택) :성별, 연령  
@@ -58,10 +80,19 @@ def default(request):
     cheap = []
     high_coverage = []
     
-    
+
+    #필요한 기본 DB 정보
+    host = "chichu" #접속할 db의 host명
+    user = "chichu" #접속할 db의 user명
+    pw = "ssafy" #접속할 db의 password
+    db = "chichu" #접속할 db의 table명 (실제 데이터가 추출되는 table)
+
+    #DB에 접속
+    conn = pymysql.connect( host= host, user = user, password = pw, db = db)
+    # Connection 으로부터 Cursor 생성 
+    curs = conn.cusor()
 
     # 3. SQL문 작성
-
     # (1) 인기 상품 : 성별 + 연령 + [가장 많이 나온 상품 순서대로]
     popular_sql = """
     """
@@ -83,40 +114,25 @@ def default(request):
 
     # (3) 치츄 지수 높은 순
     high_ci_sql = """
-    SELECT A.PRODUCT_CODE
-    FROM (
-            SELECT PRODUCT_CODE, SUM(COVERAGE) AS COVERAGE 
-            FROM PRODUCT_OPTION
-            GROUP BY PRODUCT_CODE) A
-            , PRODUCT_RATE B
-            , PRODUCT C
-    WHERE A.PRODUCT_CODE = B.PRODUCT_CODE
-    AND B.PRODUCT_CODE = C.PRODUCT_CODE
+    SELECT B.PRODUCT_CODE, TOTAL_INDEX
+    FROM PRODUCT_RATE B
+		 , PRODUCT C
+    WHERE B.PRODUCT_CODE = C.PRODUCT_CODE
     AND B.PY = 10
     AND C.SUBTYPE_CODE = 1
     AND B.AGE = 30 
     AND B.GENDER = '여'
-    ORDER BY RATE
-    LIMIT 6
+    ORDER BY TOTAL_INDEX DESC
     """
     
-    # (4) 보험료 낮은 순
+    # (4) 보험료 낮은 순 [성별, 나이, py 10, 일반형]
     cheap_sql = """
-    SELECT A.PRODUCT_CODE
-    FROM (
-            SELECT PRODUCT_CODE, SUM(COVERAGE) AS COVERAGE 
-            FROM PRODUCT_OPTION
-            GROUP BY PRODUCT_CODE) A
-            , PRODUCT_RATE B
-            , PRODUCT C
-    WHERE A.PRODUCT_CODE = B.PRODUCT_CODE
-    AND B.PRODUCT_CODE = C.PRODUCT_CODE
-    AND B.PY = 10
-    AND C.SUBTYPE_CODE = 1
-    AND B.AGE = 30 
-    AND B.GENDER = '여'
-    ORDER BY RATE
-    LIMIT 6
+    SELECT PRODUCT_CODE
+    FROM PRODUCT_RATE 
+    WHERE AGE = 30
+    AND GENDER ='여' 
+    AND PY = 10 
+    ORDER BY RATE 
     """
 
 
@@ -136,7 +152,6 @@ def default(request):
     AND B.AGE = 30 
     AND B.GENDER = '여'
     ORDER BY COVERAGE DESC
-    LIMIT 6
     """
 
     curs.execute(popular_sql)
@@ -157,28 +172,109 @@ def default(request):
     curs.close()
     conn.close()
 
-
     # 6. 5가지 조건에 맞춰서 각 보험 상품에 대한 세부 정보들은 serializer에서 표시
-    # serializer = ProductSerializer( many=True)
-    # return Response(serializer.data)
+    serializer = DefaultSerializer(popular_data,reasonable_data,high_ci_data, cheap_data,high_coverage_data, many=True)
+    return Response(serializer.data)
+
 
 
 # 2차 검색
 @api_view(['GET'])
-def detail(request):
-    
-    
-    
-    pass
+def detail(request, gender, age):
 
-# 세부 보험 상품
+    age = 19960225
+    gender = '여'
+
+    # 1. 나이 바꾸기
+    age = change_age(age)
+
+    # 2. 리스트에 담기 ?
+    high_ci = []
+    cheap = []
+    high_coverage = []
+    
+
+    #필요한 기본 DB 정보
+    host = "chichu" #접속할 db의 host명
+    user = "chichu" #접속할 db의 user명
+    pw = "ssafy" #접속할 db의 password
+    db = "chichu" #접속할 db의 table명 (실제 데이터가 추출되는 table)
+
+    #DB에 접속
+    conn = pymysql.connect( host= host, user = user, password = pw, db = db)
+    # Connection 으로부터 Cursor 생성 
+    curs = conn.cusor()
+
+
+    # (1) 치츄 지수 높은 순
+    high_ci_sql = """
+    SELECT B.PRODUCT_CODE, TOTAL_INDEX
+    FROM PRODUCT_RATE B
+		 , PRODUCT C
+    WHERE B.PRODUCT_CODE = C.PRODUCT_CODE
+    AND B.PY = 10
+    AND C.SUBTYPE_CODE = 1
+    AND B.AGE = age 
+    AND B.GENDER = gender
+    ORDER BY TOTAL_INDEX DESC
+    """
+    
+    # (2) 보험료 낮은 순 [성별, 나이, py 10, 일반형]
+    cheap_sql = """
+    SELECT PRODUCT_CODE
+    FROM PRODUCT_RATE 
+    WHERE AGE = age
+    AND GENDER = gender
+    AND PY = 10
+    ORDER BY RATE 
+    """
+
+
+    # (3) 보장금액 높은 순
+    high_coverage_sql = """
+    SELECT A.PRODUCT_CODE
+    FROM (
+            SELECT PRODUCT_CODE, SUM(COVERAGE) AS COVERAGE 
+            FROM PRODUCT_OPTION
+            GROUP BY PRODUCT_CODE) A
+            , PRODUCT_RATE B
+            , PRODUCT C
+    WHERE A.PRODUCT_CODE = B.PRODUCT_CODE
+    AND B.PRODUCT_CODE = C.PRODUCT_CODE
+    AND B.PY = 10
+    AND C.SUBTYPE_CODE = 1
+    AND B.AGE = age
+    AND B.GENDER = gender
+    ORDER BY COVERAGE DESC
+    """
+
+    curs.execute(high_ci_sql)
+    curs.execute(cheap_sql)
+    curs.execute(high_coverage_sql)
+    
+    high_ci_data = curs.fetchall()
+    cheap_data = curs.fetchall()
+    high_coverage_data = curs.fetchall()
+
+
+    serializer = DefaultSerializer(high_ci_data, cheap_data,high_coverage_data, many=True)
+    return Response(serializer.data)
+
+
+
+# 3  - 세부 보험 상품
 @api_view(['GET'])
-def product(request):
-    pass 
+def product(request, product_code):
+    # 세부 보험 상품 
+    product_code = get_object_or_404(Product, PRODUCT_CODE = product_code)
+    serializer = ProductSerializer(product_code)
+    return Response(serializer.data)
+     
 
-# 보험비교
+# 4 - 보험비교
 @api_view(['GET'])
-def compare(request):
+def compare(request, products):
+    # for i in products:
+    #     i = get_object_or_404(Product, PRODUCT_CODE = i) ? 흑흑 모르겟다
+    # 선택된 보험들에 대한 사항들
     pass 
-
-
