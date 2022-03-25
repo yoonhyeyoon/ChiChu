9e831c4398c659e4fa6ce72cb3f5c795ec22c99d
Inho <adiens916@naver.com>
2022-03-25 15:49:59 +0900

    update FE/search_result # 정렬 버튼 & 기능 추가

--- frontend/src/pages/SearchResult.tsx
+++ frontend/src/pages/SearchResult.tsx
@@ -1,23 +1,18 @@
+import { useRecoilValue } from 'recoil';
+
 import PlanBarList from '../components/SearchResult/PlanBarList/index';
 import PlanCardList from '../components/SearchResult/PlanCardList/index';
-import { PlanPreviewType } from '../types/types';
-import sample from '../components/SearchResult/sample.json';
+import SortButton from '../components/SearchResult/SortButton';
+import { planListState } from '../recoil/searchResultState';
 
 function SearchResult(): JSX.Element {
-  const planList: PlanPreviewType[] = sample;
-
-  planList.sort((a, b) => {
-    return a.plan_score > b.plan_score
-      ? -1
-      : a.plan_score < b.plan_score
-      ? 1
-      : 0;
-  });
+  const planList = useRecoilValue(planListState);
 
   return (
     <>
+      <SortButton />
       <PlanCardList list={planList.slice(0, 3)} />
-      <PlanBarList list={planList.slice(4, undefined)} />
+      <PlanBarList list={planList.slice(4)} />
     </>
   );
 }
