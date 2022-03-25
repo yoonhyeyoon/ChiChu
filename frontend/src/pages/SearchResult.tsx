df7c0cab0c1613274d1c905ca8c7b169f8a7e98f
Inho <adiens916@naver.com>
2022-03-25 20:18:27 +0900

    update FE/search_result # 목록 접기/펼치기 기능 추가

--- frontend/src/pages/SearchResult.tsx
+++ frontend/src/pages/SearchResult.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { Box, Button } from '@mui/material';
 
 import PlanBarList from '../components/SearchResult/PlanBarList/index';
 import PlanCardList from '../components/SearchResult/PlanCardList/index';
@@ -8,12 +10,25 @@ import { planListState } from '../recoil/searchResultState';
 
 function SearchResult(): JSX.Element {
   const planList = useRecoilValue<PlanPreviewType[]>(planListState);
+  const [showMore, setShowMore] = useState(false);
 
   return (
     <>
       <SortButton />
+
       <PlanCardList list={planList.slice(0, 3)} />
-      <PlanBarList list={planList.slice(4)} />
+
+      <Box textAlign="center">
+        <Button onClick={() => setShowMore(cur => !cur)}>
+          {showMore ? '접기' : '더 보기'}
+        </Button>
+      </Box>
+
+      {showMore && (
+        <>
+          <PlanBarList list={planList.slice(4)} />
+        </>
+      )}
     </>
   );
 }
