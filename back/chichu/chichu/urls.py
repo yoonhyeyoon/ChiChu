37c2df04beebe5e754eafb9a1496e19e9ece5fdc
LWY <amethyst270@naver.com>
2022-03-28 10:41:30 +0900

    url api 추가

--- back/chichu/chichu/urls.py
+++ back/chichu/chichu/urls.py
@@ -36,8 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path(r'^$', include('index.urls')),
-    path('search/', include('search.urls')),
+    path('api/search/', include('search.urls')),
 
     # swagger
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
