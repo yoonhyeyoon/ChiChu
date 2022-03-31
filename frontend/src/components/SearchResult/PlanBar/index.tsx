c7a6047080a05db6a750e1e11ce5111892c24941
Inho <adiens916@naver.com>
2022-03-31 11:20:33 +0900

    fix FE/search_result # 보험 상품 목록 타입 수정

--- frontend/src/components/SearchResult/PlanBar/index.tsx
+++ frontend/src/components/SearchResult/PlanBar/index.tsx
@@ -8,38 +8,38 @@ import {
   Stack,
 } from '@mui/material';
 
-import { PlanPreviewType } from '../../../types/types';
 import ProgressBarWithNumber from '../../Common/ProgressBarWithNumber';
+import { ProductType } from '../../../types/types';
 
-function PlanBar(props: PlanPreviewType) {
+function PlanBar({ content }: { content: ProductType }) {
   return (
     <Card>
       <CardActionArea>
         <CardHeader
           avatar={
             <Avatar
-              src={props.logo_img}
-              alt={props.brand_name}
+              src={content.company_name}
+              alt={content.company_name}
               variant="rounded"
             />
           }
-          title={props.brand_name}
-          subheader={props.plan_name}
+          title={content.company_name}
+          subheader={content.product_name}
         />
         <CardContent>
           <Stack direction="row" justifyContent="space-between" paddingX={7}>
             <Stack direction="row" spacing={4}>
               <Stack direction="column">
                 <span>설계 유형</span>
-                <h3>{props.plan_type}</h3>
+                <h3>{content.subtype_code}</h3>
               </Stack>
               <Stack direction="column">
                 <span>보험료</span>
-                <h3>{props.plan_cost}</h3>
+                <h3>{content.rate}</h3>
               </Stack>
             </Stack>
             <Box sx={{ width: '33%' }}>
-              <ProgressBarWithNumber plan_score={props.plan_score} />
+              <ProgressBarWithNumber plan_score={50} />
             </Box>
           </Stack>
         </CardContent>
--- frontend/src/components/SearchResult/PlanBarList/index.tsx
+++ frontend/src/components/SearchResult/PlanBarList/index.tsx
@@ -1,21 +1,13 @@
 import { Stack } from '@mui/material';
 
-import { PlanPreviewType } from '../../../types/types';
 import PlanBar from '../PlanBar';
+import { ProductType } from '../../../types/types';
 
-function PlanBarList({ list }: { list: PlanPreviewType[] }): JSX.Element {
+function PlanBarList({ list }: { list: ProductType[] }): JSX.Element {
   return (
     <Stack direction="column" spacing={2}>
       {list.map(content => (
-        <PlanBar
-          logo_img={content.logo_img}
-          brand_name={content.brand_name}
-          plan_name={content.plan_name}
-          plan_type={content.plan_type}
-          plan_cost={content.plan_cost}
-          plan_score={content.plan_score}
-          key={content.plan_name}
-        />
+        <PlanBar content={content} key={content.product_code} />
       ))}
     </Stack>
   );
