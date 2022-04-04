0e7da9099a29620886f52b9c21c68eb76c6168fa
yoonhyeyoon <hyeyun3306@gmail.com>
2022-04-04 19:24:32 +0900

    update 담보 icon link to tip, box shadow

--- frontend/src/components/SearchResult/PlanBar/index.tsx
+++ frontend/src/components/SearchResult/PlanBar/index.tsx
@@ -6,12 +6,14 @@ import {
   CardContent,
   CardHeader,
   Stack,
+  Typography,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import ProgressBarWithNumber from '../../Common/ProgressBarWithNumber';
 import useCheckBoxLinked from '../../../hooks/useCheckList';
 import { PlanPickerType, ProductType } from '../../../types/types';
+import { PlanCardBottomText, PlanCardLabel } from '../PlanCard/styles';
 
 function PlanBar({ content }: { content: ProductType }) {
   const planInfo: PlanPickerType = { ...content };
@@ -19,7 +21,9 @@ function PlanBar({ content }: { content: ProductType }) {
     useCheckBoxLinked();
 
   return (
-    <Card>
+    <Card
+      sx={{ borderRadius: '10px', boxShadow: 'rgb(0 0 0 / 10%) 2px 2px 20px' }}
+    >
       <CardActionArea>
         <Link
           to={`./${content.product_code}`}
@@ -34,29 +38,60 @@ function PlanBar({ content }: { content: ProductType }) {
           <CardHeader
             avatar={
               <Avatar
-                src={content.company_name}
+                src={`/images/CompanyLogo/${content.company_name}.png`}
                 alt={content.company_name}
                 variant="rounded"
+                sx={{ width: 56, height: 56 }}
               />
             }
-            title={content.company_name}
-            subheader={content.product_name}
+            // title={content.company_name}
+            title={
+              <Typography
+                sx={{
+                  fontFamily: 'NotoSansKRMedium',
+                  fontSize: '16px',
+                  color: 'black',
+                }}
+              >
+                {content.company_name}
+              </Typography>
+            }
+            // subheader={content.product_name}
+            subheader={
+              <Typography
+                sx={{
+                  fontFamily: 'NotoSansKRLight',
+                  fontSize: '15px',
+                  color: 'black',
+                }}
+              >
+                {content.product_name}
+              </Typography>
+            }
             action={<CheckBoxLinked prop={planInfo} />}
           />
           <CardContent>
             <Stack direction="row" justifyContent="space-between" paddingX={7}>
               <Stack direction="row" spacing={4}>
                 <Stack direction="column">
-                  <span>설계 유형</span>
-                  <h3>{content.subtype_code}</h3>
+                  <PlanCardLabel>설계 유형</PlanCardLabel>
+                  <PlanCardBottomText>
+                    {content.subtype_code == 1
+                      ? '실속형'
+                      : content.subtype_code == 2
+                      ? '표준형'
+                      : '고급형'}
+                  </PlanCardBottomText>
                 </Stack>
                 <Stack direction="column">
-                  <span>보험료</span>
-                  <h3>{content.rate}</h3>
+                  <PlanCardLabel>보험료</PlanCardLabel>
+                  <PlanCardBottomText>
+                    {'월 ' + content.rate.toLocaleString() + '원'}
+                  </PlanCardBottomText>
                 </Stack>
               </Stack>
               <Box sx={{ width: '33%' }}>
-                <ProgressBarWithNumber plan_score={50} />
+                <ProgressBarWithNumber plan_score={content.total_index} />
               </Box>
             </Stack>
           </CardContent>
