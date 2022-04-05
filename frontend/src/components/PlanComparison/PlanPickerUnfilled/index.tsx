827515f243d4b91623714f38190d0a1f88df1686
Inho <adiens916@naver.com>
2022-04-05 16:18:48 +0900

    design FE/plan_comparison # 상품 선택 모달 창 디자인

--- frontend/src/components/PlanComparison/PlanPickerUnfilled/index.tsx
+++ frontend/src/components/PlanComparison/PlanPickerUnfilled/index.tsx
@@ -12,7 +12,7 @@ function PlanPickerUnfilled({
     <>
       {Array.from(Array(maxNum - curNum).keys()).map(value => (
         <Grid item sm={12} md={4} key={value}>
-          <Paper>
+          <Paper elevation={3} sx={{ padding: 1, paddingBottom: 4 }}>
             <CompanyProfile
               company_name={''}
               product_name={'상품은 2 ~ 3개까지 비교 가능합니다.'}
